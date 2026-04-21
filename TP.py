de891018e3717ee7eed83709b8bcf9026a2907d8
Manuelquintana2 <manu.quintanamino@gmail.com>
2026-04-21 10:39:14 -0300

    Modularizando

--- TP.py
+++ TP.py
@@ -1,18 +1,75 @@
 import time
+import re
 # Descripción del sistema
 # El sistema está hecho para gestionar pedidos de un e-commerce,
 # incluyendo la carga de productos comprados, cantidades, 
 # métodos de envío y el seguimiento del estado de cada orden.
 
-productos = []
-producto = {}
-producto["Cantidad"] = "a"
-producto["Precio"] = "a"
-producto["MetodoDeEnvio"] = "a"
-producto["NroDeOrden"] = {"Estados" : ["Pagados", "Empaquetados", "Enviados", "Reenviado"]}
+productos_remeras = [
+    {
+        "id": 1,
+        "Nombre": "Remera Basic Oversize",
+        "Color": "Negro",
+        "Talle": "L",
+        "Precio": 6000,
+        "CantidadStock": 50
+    },
+    {
+        "id": 2,
+        "Nombre": "Remera Basic Oversize",
+        "Color": "Blanco",
+        "Talle": "M",
+        "Precio": 8000,
+        "CantidadStock": 35
+    },
+    {
+        "id": 3,
+        "Nombre": "Remera Estampada Rock",
+        "Color": "Gris Melange",
+        "Talle": "XL",
+        "Precio": 3250,
+        "CantidadStock": 15
+    },
+    {
+        "id": 4,
+        "Nombre": "Remera Deportiva Dry-Fit",
+        "Color": "Azul Francia",
+        "Talle": "S",
+        "Precio": 13000,
+        "CantidadStock": 0
+    },
+    {
+        "id": 5,
+        "Nombre": "Remera Polo Clásica",
+        "Color": "Verde Oliva",
+        "Talle": "XXL",
+        "Precio": 13000,
+        "CantidadStock": 10
+    }
+]
+
+def pedirDatos(mensaje, patron):
+    dato = input(mensaje)
+    res = validaciones(patron, dato)
+    return res
+
+def validaciones(patron,valor):
+    while not(re.match(patron,valor)):
+        print('No valido')
+        valor = input('Ingrese nuevamente: ')
+    return valor
+
+def mostrarProductos():
+    for i in range(len(productos_remeras)):
+        print(f'{i}: {productos_remeras[i]["Nombre"]}')
 
 def registrarCompras():
-    pass
+    cliente = pedirDatos("Ingrese el nombre del cliente: ", '[a-zA-Z]')
+    mostrarProductos()
+    producto = pedirDatos("Ingrese el producto que desea comprar (Seleccione un numero)", '[0-9]')
+
+
+
 
 def gestionarEstadoDePedido():
     pass
